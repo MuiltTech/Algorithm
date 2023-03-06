ec0c0580742c5384135f487db4fd66dc448a0f82
KIMSEI1124 <workju1124@gmail.com>
2023-03-06 14:12:42 +0900

    [23.03.04] : 황성민 문제 풀이 소스코드 추가

---
+++ 23.03.04/황성민/Baekjoon.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Baekjoon {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt();
+		int K = in.nextInt();
+		int A[] = new int[N];
+		int count = 0;
+		
+		for (int i=0; i<N; i++) {
+			A[i] = in.nextInt();
+		}
+		// 최대한 큰 동전 먼저 사용 
+		for (int i = N-1; i>=0; i--) { // 가장 큰 동전부터 탐색 
+			if (A[i] <= K) { // K보다 작은 동전의 종류를 탐색 
+				count += (K/A[i]); // 해당 동전의 종류로 K를 나눈 몫을 구하기 
+				K = K % A[i]; // 동전의 몫을 구하고 나눈 나머지 값을 K에 넣는다 
+			} // 이후 for 문을 돌면서K가 0이 될때까지 
+		}
+		System.out.println(count);
+	}
+
+}
